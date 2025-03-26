6b818e33cabea1492804d9fa6fda8638c739ff1b
Mateusz Cieplak <m.cieplak97@gmail.com>
2025-03-26 21:28:34 +0100

    Task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { FormEventHandler, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handlerSearch: FormEventHandler = event => {
+    setQuery(event.target.value);
+  };
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    if (
+      movie.title.toLowerCase().includes(query.toLowerCase().trim()) ||
+      movie.description.toLowerCase().includes(query.toLowerCase().trim())
+    ) {
+      return movie;
+    }
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +35,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handlerSearch}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">Sidebar goes here</div>
